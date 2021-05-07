a8b26639640931cbac7a94045a884cdcbf01e059
Kosta <68782786+KostaD02@users.noreply.github.com>
2021-05-07 22:50:50 +0400

    showing names

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log("test");
+let names = inputNames();
+
+function inputNames() {
+  let tempNames = [];
+  let playerOneName = "Player 1";
+  let playerTwoName = "Player 2";
+  tempNames.push(playerOneName, playerTwoName);
+  return tempNames;
+}
+
+function start(ClearID, ShowID) {
+  document.getElementById(ClearID).style.display = "none";
+  document.getElementById(ShowID).style.display = "inline-block";
+  displayPlayerNames();
+}
+
+function displayPlayerNames() {
+  document.getElementById("displayFirstPlayerNumber").innerHTML = names[0];
+  document.getElementById("displaySecondPlayerNumber").innerHTML = names[1];
+}
