605dd9704669de414e7d1fdfe25889aa82bb8ee4
Kosta <68782786+KostaD02@users.noreply.github.com>
2021-05-08 15:24:52 +0400

    added simplelogic

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 let names;
 let winner = false;
 const winningPlace = [
-  "1,2,3",
-  "4,5,6",
-  "7,8,9",
-  "1,4,7",
-  "2,5,8",
-  "3,6,9",
-  "1,5,9",
-  "3,5,7",
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7],
 ];
 
 let x = [];
@@ -68,7 +68,6 @@ function placerLogic(displayID, char) {
         ).innerHTML = `<i class="far fa-circle"></i>`;
         y.push(`${displayID}`);
       }
-
       list[`${displayID}`] = false;
       list.counter++;
     }
@@ -125,7 +124,7 @@ function checkWinner(counter) {
     let currentString = `${xArray[0]},${xArray[1]},${xArray[2]}`;
     for (let i = 0; i < winningPlace.length; i++) {
       for (let j = 0; j < 3; j++) {
-        if (winningPlace[i] == currentString) {
+        if (winningPlace[i].toString() == currentString) {
           win = true;
           break;
         }
@@ -142,7 +141,7 @@ function checkWinner(counter) {
     let currentString = `${yArray[0]},${yArray[1]},${yArray[2]}`;
     for (let i = 0; i < winningPlace.length; i++) {
       for (let j = 0; j < 3; j++) {
-        if (winningPlace[i] == currentString) {
+        if (winningPlace[i].toString() == currentString) {
           win = true;
           break;
         }
@@ -153,6 +152,62 @@ function checkWinner(counter) {
       winner = true;
     }
   }
+  if (counter == 7) {
+    let xArray = x.sort();
+    let counter = 0;
+    for (let i = 0; i < winningPlace.length; i++) {
+      for (let j = 0; j < 4; j++) {
+        if (winningPlace[i][j] == xArray[j]) {
+          counter++;
+          if (counter == 3) {
+            displayWinner(1);
+            winner = true;
+            break;
+          }
+        }
+      }
+      counter = 0;
+    }
+  }
+  if (counter == 8) {
+    let yArray = y.sort();
+    let counter = 0;
+    for (let i = 0; i < winningPlace.length; i++) {
+      for (let j = 0; j < 4; j++) {
+        if (winningPlace[i][j] == yArray[j]) {
+          counter++;
+          if (counter == 3) {
+            displayWinner(2);
+            winner = true;
+            break;
+          }
+        }
+      }
+      counter = 0;
+    }
+  }
+  if (counter == 9) {
+    let xArray = x.sort();
+    let counter = 0;
+    for (let i = 0; i < winningPlace.length; i++) {
+      for (let j = 0; j < 5; j++) {
+        if (winningPlace[i][j] == xArray[j]) {
+          counter++;
+          if (counter == 3) {
+            displayWinner(1);
+            winner = true;
+            return;
+          }
+        }
+      }
+      counter = 0;
+    }
+    Swal.fire({
+      icon: "info",
+      title: "Oops...",
+      text: "Tie , there is no winner",
+    });
+  }
 }
 
 function displayPoint(displayID) {
@@ -160,5 +215,8 @@ function displayPoint(displayID) {
   placerLogic(displayID, char);
   if (list.counter == 5) checkWinner(5);
   if (list.counter == 6) checkWinner(6);
+  if (list.counter == 7) checkWinner(7);
+  if (list.counter == 8) checkWinner(8);
+  if (list.counter == 9) checkWinner(9);
   guessingMove(char);
 }
